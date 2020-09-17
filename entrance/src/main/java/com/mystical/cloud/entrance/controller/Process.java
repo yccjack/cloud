17e32201e5a6385b2a1edbc835b2f2c615128e40
MysticalYcc <568166723@qq.com>
2020-09-17 16:26:26 +0800

    增加注册中心，增加服务测试网关

--- entrance/src/main/java/com/mystical/cloud/entrance/controller/Process.java
+++ entrance/src/main/java/com/mystical/cloud/entrance/controller/Process.java
@@ -1,6 +1,8 @@
 package com.mystical.cloud.entrance.controller;
 
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -9,9 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("process")
 public class Process {
-    @RequestMapping("startEvent")
-    public String startEvent(){
-
+    @RequestMapping(value = "startEvent" ,  method = RequestMethod.POST)
+    public String startEvent(@RequestBody String json){
+        System.out.println(json);
         return "ok";
     }
 }
