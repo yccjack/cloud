d557d21229cf972d10d3cdb2367798092d03b89a
MysticalYcc <15155137510@163.com>
2021-09-26 16:28:23 +0800

    修复doc.html

--- auth/src/main/java/com/mystical/cloud/auth/AuthApplication.java
+++ auth/src/main/java/com/mystical/cloud/auth/AuthApplication.java
@@ -3,13 +3,22 @@ package com.mystical.cloud.auth;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @MapperScan("com.mystical.cloud.auth.mapper")
-public class AuthApplication {
+public class AuthApplication extends SpringBootServletInitializer implements WebMvcConfigurer {
 
     public static void main(String[] args) {
         SpringApplication.run(AuthApplication.class, args);
     }
 
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("doc.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+        registry.addResourceHandler("/favicon.ico").addResourceLocations("classpath:/ico/");
+    }
 }
